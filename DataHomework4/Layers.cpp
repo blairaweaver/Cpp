ddc425b2f806d244260d4fa7f60de56ccb3a3a0d
blairaweaver <blairaweaver@yahoo.com>
2020-02-18 20:53:07 -0500

    Just a push

---
+++ DataHomework4/Layers.cpp
@@ -0,0 +1,45 @@
+//
+// Created by blair on 2/18/2020.
+//
+#include "stdio.h"
+
+void applicationLayer(char *buffer, int msize){
+
+    presentationLayer()
+}
+
+void presentationLayer(char *buffer, int msize){
+
+    sessionLayer()
+}
+
+void sessionLayer(char *buffer, int msize){
+
+    transportLayer()
+}
+
+void transportLayer(char *buffer, int msize){
+
+    networkLayer()
+}
+
+void networkLayer(char *buffer, int msize){
+
+    dataLinkLayer()
+}
+
+void dataLinkLayer(char *buffer, int msize){
+
+    physicalLayer()
+}
+
+void physicalLayer(char *buffer, int msize){
+
+
+}
+
+
+int main() {
+
+    applicationLayer()
+}
